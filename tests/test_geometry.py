d10db073cba45b56b2c193dff600d0a351acb2b4
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2023-04-04 15:51:57 -0600

    added exception test for global_to_local()

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -13,11 +13,15 @@ class TestGlobalToLocalValues(unittest.TestCase):
         self.z = 3.0
         self.z_e = np.array([0, 6])
 
-    def test_value(self):
+    def test_valid_float_input(self):
         s_exp = 0.5
         s_act = global_to_local(self.z, self.z_e)
-        self.assertAlmostEqual(s_exp, s_act)
         self.assertIsInstance(s_act, float)
+        self.assertAlmostEqual(s_exp, s_act)
+
+    def test_invalid_str_input(self):
+        with self.assertRaises(TypeError):
+            global_to_local('two', self.z_e)
 
 
 if __name__ == "__main__":
