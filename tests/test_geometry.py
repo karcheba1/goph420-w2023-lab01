6d15277361499b7fa404eb6f9705c50d111bb505
Zachary Gilchrist <zachary.gilchrist@ucalgary.ca>
2023-04-04 16:39:06 -0600

    in progress testing for gradient matrix

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -3,7 +3,7 @@ import unittest
 import numpy as np
 
 from fem_1d_heat.geometry import (
-        global_to_local,
+        global_to_local, gradient_matrix
 )
 
 
@@ -23,6 +23,32 @@ class TestGlobalToLocalValues(unittest.TestCase):
         with self.assertRaises(TypeError):
             global_to_local('two', self.z_e)
 
+
+class TestGradientMatrix(unittest.TestCase):
+    """This class is setup for the linear case such that the s parameter is unused and such we feed a placeholder value of zero into it."""
+    def setUp(self):
+        self.s = 0
+        self.dz = 2
+
+    def test_valid_float_input(self):
+        g_exp = np.ndarray([-.5,.5])
+        g_act = gradient_matrix(self.s, self.dz)
+        self.assertIsInstance(g_act, np.ndarray)
+        self.assertTrue(np.allclose(g_exp,g_act))
+
+    def test_valid_string_input(self):
+        g_exp = np.ndarray([-0.2,0.2])
+        g_act = gradient_matrix(0, '5')
+        self.assertIsInstance(g_act, np.ndarray)
+        self.assertTrue(np.allclose(g_exp,g_act))
+
+    def test_correct_shape(self):
+        g_act = gradient_matrix(self.s, self.dz)
+        self.assertTrue(g_act.shape()==(1,2))
+    
+    def test_invalid_str_input(self):
+        with self.assertRaises(ValueError):
+            gradient_matrix('five', self.dz)
 
 if __name__ == "__main__":
     unittest.main()
